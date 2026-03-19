c5791a1879ebde1bd1d849b4436b11a19a796e66
Natan <natanrozycki189@gmail.com>
2026-03-19 21:28:26 +0100

    Almost finished the code, added a new void function, a way to test the entire program and a way for the shifting to ignore spaces.

--- Scrambler.cpp
+++ Scrambler.cpp
@@ -5,13 +5,24 @@ std::string Cipher(std::string text, int startershift) {
 
 
     for (int i = 0; i < text.length(); i++) {
+        if (text[i] == ' ') continue;
         text[i] = text[i] + startershift;
         startershift++;
     }
     return text;
 }
+void showCipher(std::string text) {
+    std::cout << "Your ciphered text: " << text << std::endl;
+}
 
 int main() {
 
-    std::string original = "Hello world";
+    std::string original;
+
+    std::cout << "Enter any text: ";
+    std::getline(std::cin, original);
+
+    std::string newstring = Cipher(original, 2);
+    showCipher(newstring);
+    return 0;
 }
