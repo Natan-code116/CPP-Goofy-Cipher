fafcc0295efb9dacab7d961a46d4881e85d5d5da
Natan <natanrozycki189@gmail.com>
2026-03-19 21:54:51 +0100

    Huge update- added a decryption function, a way for the user to choose between decrypting and exiting the program, also included a failsafe for the character overflow in character shift loop.

--- Scrambler.cpp
+++ Scrambler.cpp
@@ -4,25 +4,62 @@
 std::string Cipher(std::string text, int startershift) {
 
 
-    for (int i = 0; i < text.length(); i++) {
-        if (text[i] == ' ') continue;
-        text[i] = text[i] + startershift;
-        startershift++;
-    }
-    return text;
+	for (int i = 0; i < text.length(); i++) {
+		if (text[i] == ' ') continue;
+		// added a failsafe
+		text[i] = (text[i] - 32 + startershift) % 95 + 32;
+		startershift++;
+	}
+	return text;
 }
+
+std::string Decipher(std::string text, int startershift) {
+	for (int i = 0; i < text.length(); i++) {
+		if (text[i] == ' ') continue;
+		// Subtract the shift to go backwards
+		text[i] = text[i] - startershift;
+		startershift++;
+	}
+	return text;
+}
+
 void showCipher(std::string text) {
-    std::cout << "Your ciphered text: " << text << std::endl;
+	std::cout << "Your ciphered text: " << text << std::endl;
 }
 
 int main() {
+	char choice;
 
-    std::string original;
+	do {
+		std::string original, currentResult;
 
-    std::cout << "Enter any text: ";
-    std::getline(std::cin, original);
+		std::cout << "Enter any text: ";
+		std::getline(std::cin, original);
 
-    std::string newstring = Cipher(original, 2);
-    showCipher(newstring);
-    return 0;
+		std::string newstring = Cipher(original, 2);
+		showCipher(newstring);
+
+		std::cout << "Choose the next option [DECRYPT] / [EXIT]: ";
+		std::cin >> choice;
+		std::cin.ignore();
+		//decryption
+		if (choice == 'd') {
+			int shifts;
+			std::string encword;
+
+			std::cout << "Enter the encrypted message: ";
+			std::getline(std::cin, encword);
+
+			std::cout << "Enter the number of shifts desired: ";
+			std::cin >> shifts;
+
+			std::string fix = Decipher(encword, shifts);
+			std::cout << "Your Deciphered text is: " << fix << std::endl;
+			break;
+		}
+		//manual exit
+	} while (choice != 'd');
+		std::cout << "Exiting..." << std::endl;
+		return 0;
+	
 }
