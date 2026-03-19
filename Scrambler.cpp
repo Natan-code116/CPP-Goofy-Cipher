f50fee4c0707e722ae151ef75818d982d5f65e74
Natan <natanrozycki189@gmail.com>
2026-03-19 20:57:11 +0100

    Updated the code to be more fluid, therefore i added a function instead of having a raw string sitting there. -small update + code currently doesn't work because its heavily unfinished.

--- Scrambler.cpp
+++ Scrambler.cpp
@@ -1,16 +1,17 @@
 #include <iostream>
 #include <string>
+// added a function for more variety of texts
+std::string Cipher(std::string text, int startershift) {
+
+
+    for (int i = 0; i < text.length(); i++) {
+        text[i] = text[i] + startershift;
+        startershift++;
+    }
+    return text;
+}
 
 int main() {
-	std::string text = "Hello world!";
-	int shift = 2;
 
-	std::cout << "Original text: " << text << std::endl;
-
-	for (int i = 0; i < text.length(); i++) {
-		text[i] = text[i] + shift;
-		shift++;
-	}
-
-	std::cout << "Cipher/Scrambled text: " << text << std::endl;
+    std::string original = "Hello world";
 }
