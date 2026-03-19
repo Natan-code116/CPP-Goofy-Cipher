aada4f96d3983347e830aa89358094a564fe3b98
Natan <natanrozycki189@gmail.com>
2026-03-19 20:40:21 +0100

    Added a string variable, the loop that ciphers every letter one by one and a comparison text showing both texts (Normal and Ciphered). Might change this later.

--- Scrambler.cpp
+++ Scrambler.cpp
@@ -1,1 +1,16 @@
 #include <iostream>
+#include <string>
+
+int main() {
+	std::string text = "Hello world!";
+	int shift = 2;
+
+	std::cout << "Original text: " << text << std::endl;
+
+	for (int i = 0; i < text.length(); i++) {
+		text[i] = text[i] + shift;
+		shift++;
+	}
+
+	std::cout << "Cipher/Scrambled text: " << text << std::endl;
+}
